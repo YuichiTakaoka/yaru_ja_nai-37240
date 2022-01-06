fdc0f1a74a1ca0afbc6f70eaba10bf4fa1123479
YuichiTakaoka <impression96722697@gmail.com>
2022-01-06 12:31:28 +0900

    プロフィール編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'profiles/index'
   root to: "profiles#index"
-    resources :profiles, only: [:new, :create, :show]
+    resources :profiles, only: [:new, :create, :show, :edit, :update]
 end
